79f313ef84ee3c5215b0942ab3eb4165304817ed
Your Name <you@example.com>
2023-08-17 19:57:25 +0100

    Give canvas context and 2D setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 let playerSprite = document.getElementById("sprite");
+
+// Setting up the canvas to have context and be drawn on 
+let canvas;
+let context;
+
+window.onload = contextForCanvas;
+
+function contextForCanvas(){
+        canvas = document.getElementById("canvas");
+        context = canvas.getContext("2d");
+        
+        draw();
+}
