aec3396817a7606bab283f678589c1aca1ebd5fb
Your Name <you@example.com>
2023-08-18 22:02:26 +0100

    Set canvas size, onload event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
-let playerSprite = document.getElementById("sprite");
+
 
 // Setting up the canvas to have context and be drawn on 
 let canvas;
 let context;
 
-window.onload = contextForCanvas;
+canvas = document.getElementById("canvas");
+context = canvas.getContext("2d");
+let playerSprite = document.getElementById("sprite");
+
+
+
+window.onload = function() {
+    //Page is now loaded 
+    let playerSprite = document.getElementById("sprite");
+    context.drawImage(playerSprite, 10,10);
+}
 
 function contextForCanvas(){
         canvas = document.getElementById("canvas");
@@ -12,3 +22,11 @@ function contextForCanvas(){
         
         draw();
 }
+
+/*function draw(){
+    let placeholderBackground = '#0095b0';
+    context.fillStyle = placeholderBackground;
+    context.fillRect(100,50,200,175);
+}*/
+
+
