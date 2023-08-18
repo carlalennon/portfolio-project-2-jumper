b07a103e3ce94089e8af21c721ca41410b035dcd
Your Name <you@example.com>
2023-08-19 00:06:24 +0100

    Bugfix canvas was not drawn

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,29 @@ function startGame(){
 
 }
 
+var  myGameArea = {
+    //Creates canvas element 
+    canvas : document.createElement("canvas"),
+
+    start : function() {
+        // Change canvas size to window width after testing 
+        this.canvas.width = 400;
+        this.canvas.height = 400;
+
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener("keydown", function (e) {
+            myGameArea.key = e.key;
+        })
+    },
+    clear : function() {
+        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+    }
+}
+
+
 // Give the player object context so it knows what it is 
 
 function component(width, height, color, x, y) {
@@ -32,30 +55,20 @@ function component(width, height, color, x, y) {
     }
 }
 
+
 // Add frames to game 50fps
 
 
-var  myGameArea = {
-    canvas : document.createElement("canvas"),
-
-    start : function() {
-        // Change canvas size to window width after testing 
-        this.canvas.width = 400;
-        this.canvas.height = 400;
-
-        this.context = this.canvas.getContext("2d");
-        this.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function() {
-        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
-    }
-}
 
 // Update function. Sets and clears game area 50fps
 
 function updateGameArea() {
     myGameArea.clear();
+    myGamePiece.speedX=0;
+    myGamePiece.speedY=0;
+    //Adding arrow control 
+    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY=-1;};
+    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedX=1};
     myGamePiece.newPos();
     myGamePiece.update();
 }
@@ -70,13 +83,10 @@ function movedown() {
     myGamePiece.speedY += 1;
 }
 
-function moveleft() {
-    myGamePiece.speedX -= 1;
-}
+// when player is ducking, his height needs to be half and a new sprite needs to be drawn
 
-function moveright() {
-    myGamePiece.speedX += 1;
-}
+// Adding event listeners 
+
 
 // Setting up the canvas to have context and be drawn on 
 
