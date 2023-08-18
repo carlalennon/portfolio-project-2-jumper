8612910af93c565e70a64b19706a140eff412d7c
Your Name <you@example.com>
2023-08-18 23:12:46 +0100

    Draw component to canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,30 @@
 
 // set up Canvas 
 
+var player;
+
 function startGame(){
     myGameArea.start();
+
+    //placeholder for player sprite
+    player = new component(30,30,"red",10,120);
+
 }
+
+// Give the player object context so it knows what it is 
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    ctx = myGameArea.context;
+    ctx.fillStyle - color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
+// Add frames to game 50fps
+
 
 var  myGameArea = {
     canvas : document.createElement("canvas"),
@@ -14,42 +35,20 @@ var  myGameArea = {
 
         this.context = this.canvas.getContext("2d");
         this.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
     }
-
 }
-// Setting up the canvas to have context and be drawn on 
-let canvas;
-let context;
 
-canvas = document.getElementById("canvas");
-context = canvas.getContext("2d");
-let playerSprite = document.getElementById("sprite");
+// Setting up the canvas to have context and be drawn on 
+
 
 // Canvas height 1080
 
 
-// Add frames to game 
 
 
-
-window.onload = function() {
-    //Page is now loaded 
-    let playerSprite = document.getElementById("sprite");
-    context.drawImage(playerSprite, 50,900);
-}
-
-function contextForCanvas(){
-        canvas = document.getElementById("canvas");
-        context = canvas.getContext("2d");
-        
-        draw();
-}
-
-
-/*function draw(){
-    let placeholderBackground = '#0095b0';
-    context.fillStyle = placeholderBackground;
-    context.fillRect(100,50,200,175);
-}*/
 
 
